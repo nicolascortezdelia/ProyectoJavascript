72c64ff22fbd5fdd0b0f003c02d7ee01de80959b
Nicolas Cortez D Elia <nicocdelia@gmail.com>
2021-09-27 11:08:47 -0300

    Variables

---
+++ js/holamundo.js
@@ -0,0 +1,37 @@
+//Así se ponen los comentarios
+
+//mostrar un mensaje en la pantalla
+document.write("Hola Mundo");
+
+/*Mostrar un mensaje en la consola*/
+
+console.log("Hola Mundo");
+
+/*Mostrar un mensaje en una ventana emergente */
+
+//alert("Hola comisión 18i");
+
+//crear (o declarar O Inicializar) una variable (var - let - const)
+
+let persona = "Nico Cortez";
+
+document.write("<br>Persona: " + persona)
+
+//Modificar el valor de una variable
+
+persona = "Gabriel Batistuta";
+
+document.write("<br>Persona: " + persona);
+
+let edad = 25;
+
+document.write("<br>Edad:" + edad)
+
+// Crear Variables CONSTANTES (que no se va a modificar nunca)
+
+const URL = "https://campus.rollingcodeschool.com/"
+
+document.write ("<br> Link:" + URL);
+
+//
+

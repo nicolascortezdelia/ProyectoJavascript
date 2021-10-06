6be4aa94219f2d7d24fed4dce31fceb61267c542
Nicolas Cortez D Elia <nicocdelia@gmail.com>
2021-10-06 11:30:48 -0300

    Agregué la página arreglos

---
+++ js/5.-arreglos.js
@@ -0,0 +1,62 @@
+// Cómo crear un arreglo con datos?
+let series = ["Juego del Calamar", "Loki", "Frozen", 2, true];
+
+//Cómo crear un Arreglo vacío
+let temporadas = [];
+
+//cómo mostrar un valor de un arreglo
+
+document.write (series[0]);
+
+//cómo saber cuántos elementos tiene el Arreglo?
+
+console.log (series.length);
+
+//cómo recorrer elemento por elemento un Arreglo
+for(let posicion = 0; posicion < series.length ; posicion++){
+    document.write("<br>" +series[posicion]);
+}
+
+// cómo agregar un elemento al Final de un arreglo
+
+series.push("Breaking Bad");
+
+document.write("<br><b>Arreglo series con nuevo Elemento</b><br><hr>");
+
+for(let posicion = 0; posicion < series.length ; posicion++){
+    document.write("<br>" +series[posicion]);
+}
+
+
+//Cómo agregar un Elemento en una posición particular
+
+series.splice(1, 0, "GOT");// (posición elemento, cantidad de elementos a borrar, el elemento a agregar)
+
+document.write("<br><b>Arreglo series con nuevo Elemento en una Posición particular</b><br><hr>");
+
+for(let posicion = 0; posicion < series.length ; posicion++){
+    document.write("<br>" +series[posicion]);
+}
+
+
+
+//Cómo eliminar un elemento de un Arreglo?
+
+series.splice (5,1,); //(posición del elemento, cantidad de elementos a borrar)
+// si pongo: series.splice (5) BORRO TODO desde el elemento 5
+
+document.write("<br><b>ELIMINAR un elemento de una Posición particular</b><br><hr>");
+
+for(let posicion = 0; posicion < series.length ; posicion++){
+    document.write("<br>" +series[posicion]);
+}
+
+//Cómo cambiar el valor de un Arreglo
+
+series[4] = "Friends";
+document.write("<br><b>Cambiar el valor de un elemento de un Arreglo</b><br><hr>");
+
+for(let posicion = 0; posicion < series.length ; posicion++){
+    document.write("<br>" +series[posicion]);
+}
+
---
+++ js/tp9.js
@@ -0,0 +1,11 @@
+let frase = prompt ("Ingrese una Frase");
+
+console.log (frase);
+console.log (frase.length);
+console.log (frase.charAt(1));
+
+// recorrer la "frase" con un bucle (while o for)
+// recorrer hasta el fin del bucle---> frase.length
+//if ----> preguntar si cada caracter es una vocal (frase.charAt (x)) == "a" || (frase.charAt (x)) == "e"
+
+//mostrar con un document.write (X)

7920519610c0c649abb690b1df895924467cacff
Nicolas Cortez D Elia <nicocdelia@gmail.com>
2021-10-09 14:58:16 -0300

    Agruegué carpeta tp2

---
+++ js/Tp3/Ejercicio1Tp3.js
@@ -0,0 +1,9 @@
+let meses = ["Enero", "Febrero", "Marzo", "Abril", "Junio", "Julio","Agosto", "Septiembre", "Octubre","Noviembre", "Diciembre"];
+
+console.log (meses.length);
+
+for(let posicion = 0; posicion < meses.length ; posicion++){
+    document.write("<br>" +meses[posicion]);
+}
+
+
---
+++ js/Tp3/Ejercicio3tp3.js
@@ -0,0 +1,5 @@
+let numeros = Math.random () * (7 - 1) + 1;
+
+alert (numeros)
+
+//let resultados = [3.2854623917184704 + 4.9257929742349855 ]

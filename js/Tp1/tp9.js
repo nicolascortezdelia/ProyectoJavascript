e560a7ef0c28b6f184ae9548c5d08ff01f67a0a5
Nicolas Cortez D Elia <nicocdelia@gmail.com>
2021-10-12 21:49:52 -0300

    Ejercicio 9 tp1

--- js/Tp1/tp9.js
+++ js/Tp1/tp9.js
@@ -3,9 +3,19 @@ let frase = prompt ("Ingrese una Frase");
 console.log (frase);
 console.log (frase.length);
 console.log (frase.charAt(1));
+console.log (frase.toLowerCase)
+
+frase = frase.toLowerCase();
 
 // recorrer la "frase" con un bucle (while o for)
 // recorrer hasta el fin del bucle---> frase.length
 //if ----> preguntar si cada caracter es una vocal (frase.charAt (x)) == "a" || (frase.charAt (x)) == "e"
 
 //mostrar con un document.write (X)
+
+
+for (let i=0; i<(frase.length); i = i + 1){ //for (declaro variable i, asigno valor 0; condición lógica: programa va a funcionar MIENTRAS i sea menor que el número de caracteres; cuánto quiero que se vaya incrementando el valor de i)
+    if(frase.charAt(i) == "a" || frase.charAt(i) == "e" || frase.charAt(i) == "i" || frase.charAt(i) == "o" || frase.charAt(i) == "u" || frase.charAt(i) == "á" || frase.charAt(i) == "é" || frase.charAt(i) == "í" ||  frase.charAt(i) == "ó" || frase.charAt(i) == "ú"){
+        document.write (frase.charAt(i));
+    }
+}

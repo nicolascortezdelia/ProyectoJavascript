ffc5bddff2ea7a38ea0f47f87f03b2d0cc35cb1c
Nicolas Cortez D Elia <nicocdelia@gmail.com>
2021-10-18 12:40:51 -0300

    Agrego archivo objetos

---
+++ js/6.-funciones.js
@@ -0,0 +1,11 @@
+//funciones sin parámetro
+
+function saludar(){
+    //todo el código que quiero que tenga la función
+    //puedo poner condicionales, bucles, etc, etc
+    document.write("hola mundo");
+}
+
+// invocar o llamar una funcion
+
+saludar();
---
+++ js/7.-Objetos.js
@@ -0,0 +1,61 @@
+/*let email = "jonyplo@gmail.com";
+let nombre = "jony plodzien";
+let domicilio = "Dirección de Jony";
+let localidad = "4000";
+let password = "A123$456";*/
+
+//1.-crear un Objeto con NOTACIÓN LITERAL
+
+let usuarioJony = {
+    //propiedades---> clave:valor
+    email: "jonyplo@gmail.com", 
+    nombre: "Jony Plodzien",
+    domicilio: "domicilio de Jony",
+    localidad: "4000",
+    estado: true,
+
+    //métodos o funciones
+    /*login: function () {
+        document.write("<br>Usuario logueado");
+    } */
+    login: ()=>{
+        document.write("<br>Usuario logueado");
+
+    }
+
+
+
+}
+
+//¿Cómo MOSTRAR un objeto?
+
+console.log(usuarioJony);
+
+//FORMA Nº1
+document.write("Email: "+ usuarioJony.email);
+document.write("<br>Localidad: "+ usuarioJony.localidad);
+
+//FORMA Nº2
+
+document.write("<br>Nombre: "+ usuarioJony["nombre"]);
+
+//¿cómo MODIFICAR una propiedad del objeto???
+
+usuarioJony.localidad = "2000"
+document.write("<br>Localidad: "+ usuarioJony.localidad);
+
+//¿¿¿¿¿CÓMO AGREGAR UNA NUEVA Propiedad al OBJETO???
+
+usuarioJony.nivel = 1;
+document.write("<br>Nivel: "+ usuarioJony.nivel);
+
+//¿¿cómo BORRAR una Propiedad de un Objeto????
+delete usuarioJony.estado;
+
+//CÓMO LLAMAR UN MÉTODO
+
+usuarioJony.login()
+
+
+
+
